8252763d0cd7e31603d471ddd255213599383483
Martin Väth <martin@mvath.de>
2017-11-09 08:18:06 +0100

    Strip descriptions. Only left mouse button. Serialize, Avoid global variabels

--- data/panel/dupes.js
+++ data/panel/dupes.js
@@ -2,15 +2,14 @@
  * This project is under the GNU public license 2.0
 */
 
-"use strict";
+// For documentation on the bookmark API see e.g.
+// https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/bookmarks/
 
-var calculating;
-var lock = false;
+"use strict";
 
 function displayMessage(msg) {
   document.getElementById("textMessage").innerHTML = msg;
 }
-
 
 function getButtonsExtra() {
   return document.getElementById("buttonsExtra");
@@ -46,7 +45,7 @@ function addButtons(mode) {
   }
   addButton(parent, "buttonUnmarkAll");
   parent = getButtonsRemove();
-  addButton(parent, "buttonRemoveMarked");
+  addButton(parent, (mode == 2) ? "buttonStripMarked" : "buttonRemoveMarked");
 }
 
 function clearItem(top) {
@@ -62,6 +61,11 @@ function clearButtonsExtra() {
 
 function clearBookmarks() {
   clearItem(getTop());
+}
+
+function clearWindow() {
+  clearButtonsExtra();
+  clearBookmarks();
 }
 
 function addRuler() {
@@ -124,226 +128,300 @@ function mark(mode) {
   }
 }
 
-function calculateDupe(node, parent) {
-  if ((!node.url) || (node.type && node.type != "bookmark") || !parent) {
-    return;
-  }
-  let groupIndex = node.url;
-  let extra;
-  if (!calculating.exact) {
-    let index = groupIndex.indexOf("?");
-    if (index > 0) {
-      groupIndex = groupIndex.substring(0, index);
-      extra = node.url.substring(index);
-    }
-  }
-  let id = node.id;
-  let group = calculating.map.get(groupIndex);
-  if (typeof(group) == "undefined") {
-    group = {
-      data : new Array(),
-      ids : new Set()
-    };
-    calculating.array.push(group);
-    calculating.map.set(groupIndex, group);
-  } else if (group.ids.has(id)) {
-    return;
-  }
-  group.ids.add(id);
-  let bookmark = {
-    id: id,
-    text: parent + node.title
-  };
-  if (typeof(extra) != "undefined") {
-    bookmark.extra = extra;
-  }
-  group.data.push(bookmark);
-}
+function calculate(mode, callback) {
+  let exact, dupes, empty, all, result, urlMap;
 
-function calculateEmptyFolder(node, parent) {
-  if (node.url || (node.type && (node.type != "folder")) || !parent) {
-    return;
-  }
-  let bookmark = {
-    id: node.id,
-    text: parent + node.title
-  };
-  calculating.array.push(bookmark);
-}
-
-function calculateRecurse(node, parent) {
-  if (node.unmodifiable || (node.type === "separator")) {
-    return;
-  }
-  if (calculating.dupes) {
-    calculateDupe(node, parent);
-  }
-  if ((!node.children) || (!node.children.length)) {
-    if (!calculating.dupes) {
-      calculateEmptyFolder(node, parent);
+  function handleDupe(node, parent) {
+    if ((!node.url) || (node.type && node.type != "bookmark")
+      || node.unmodifiable || !parent) {
+      return;
     }
-    return;
-  }
-  if (node.title) {
-    parent += node.title + " | ";
-  }
-  for (let child of node.children) {
-    calculateRecurse(child, parent);
-  }
-}
-
-function calculateFinish() {
-  calculating = {};
-}
-
-function calculateDupes(nodes) {
-  calculating.dupes = true;
-  calculating.map = new Map();
-  calculating.array = new Array();
-  calculateRecurse(nodes[0], "");
-  let total = 0;
-  let groups = 0;
-  for (let group of calculating.array) {
-    if (group.data.length < 2) {
-      continue;
-    }
-    if (groups++ == 0) {
-      addButtons(0);
-    } else {
-      addRuler();
-    }
-    for (let bookmark of group.data) {
-      ++total;
-      let text = bookmark.text;
-      if (typeof(bookmark.extra) != "undefined") {
-        text += " (" + bookmark.extra + ")";
+    let groupIndex = node.url;
+    let extra;
+    if (!exact) {
+      let index = groupIndex.indexOf("?");
+      if (index > 0) {
+        groupIndex = groupIndex.substring(0, index);
+        extra = node.url.substring(index);
       }
-      addBookmark(text, bookmark.id, false);
+    }
+    let id = node.id;
+    let group = urlMap.get(groupIndex);
+    if (typeof(group) == "undefined") {
+      group = {
+        data : new Array(),
+        ids : new Set()
+      };
+      result.push(group);
+      urlMap.set(groupIndex, group);
+    } else if (group.ids.has(id)) {
+      return;
+    }
+    group.ids.add(id);
+    let bookmark = {
+      id: id,
+      text: parent + node.title
+    };
+    if (typeof(extra) != "undefined") {
+      bookmark.extra = extra;
+    }
+    group.data.push(bookmark);
+  }
+
+  function handleEmpty(node, parent) {
+    if (((typeof(node.url) != undefined) && (node.url != null))
+      || (node.type && (node.type != "folder"))
+      || node.unmodifiable || !parent) {
+      return;
+    }
+    let bookmark = {
+      id: node.id,
+      text: parent + node.title
+    };
+    result.push(bookmark);
+  }
+
+  function handleAll(node, parent, index) {
+    if ((typeof(node.url) == undefined) || (node.url == null)
+      || (node.type && node.type != "bookmark")
+      || node.unmodifiable || !parent) {
+      return;
+    }
+    let bookmark = {
+      id: node.id,
+      text: parent + node.title,
+      index: index
+    };
+    result.push(bookmark);
+  }
+
+  function recurse(node, parent = "", index = 0) {
+    if (all) {
+      handleAll(node, parent, index);
+    } else if (dupes) {
+      handleDupe(node, parent);
+    }
+    if ((!node.children) || (!node.children.length)) {
+      if (empty) {
+        handleEmpty(node, parent);
+      }
+      return;
+    }
+    if (node.title) {
+      parent += node.title + " | ";
+    }
+    index = 0;
+    for (let child of node.children) {
+      recurse(child, parent, ++index);
     }
   }
-  displayMessage(browser.i18n.getMessage((calculating.exact ?
-    "messageExactMatchesGroups" : "messageSimilarMatchesGroups"),
-    [String(total), String(groups)]));
-  calculateFinish();
-}
 
-function calculateEmpty(nodes) {
-  calculating.dupes = false;
-  calculating.array = new Array();
-  calculateRecurse(nodes[0], "");
-  let total = calculating.array.length;
-  if (total) {
-    addButtons(1);
-    for (let bookmark of calculating.array) {
-      addBookmark(bookmark.text, bookmark.id, false);
+  function calculateDupes(nodes) {
+    dupes = true;
+    urlMap = new Map();
+    result = new Array();
+    recurse(nodes[0]);
+    let total = 0;
+    let groups = 0;
+    for (let group of result) {
+      if (group.data.length < 2) {
+        continue;
+      }
+      if (groups++ == 0) {
+        addButtons(0);
+      } else {
+        addRuler();
+      }
+      for (let bookmark of group.data) {
+        ++total;
+        let text = bookmark.text;
+        if (typeof(bookmark.extra) != "undefined") {
+          text += " (" + bookmark.extra + ")";
+        }
+        addBookmark(text, bookmark.id, false);
+      }
     }
+    displayMessage(browser.i18n.getMessage((exact ?
+      "messageExactMatchesGroups" : "messageSimilarMatchesGroups"),
+      [String(total), String(groups)]));
   }
-  displayMessage(browser.i18n.getMessage("messageEmpty", String(total)));
-  calculateFinish();
-}
 
-function calculateError(error) {
-  displayMessage(browser.i18n.getMessage("messageCalculatingError", error));
-  calculateFinish();
-}
+  function calculateEmpty(nodes) {
+    empty = true;
+    result = new Array();
+    recurse(nodes[0]);
+    let total = result.length;
+    if (total) {
+      addButtons(1);
+      for (let bookmark of result) {
+        addBookmark(bookmark.text, bookmark.id, false);
+      }
+    }
+    displayMessage(browser.i18n.getMessage("messageEmpty", String(total)));
+  }
 
-function calculate(mode) {
+  function calculateAll(nodes) {
+    all = true;
+    result = new Array();
+    recurse(nodes[0]);
+    let total = result.length;
+    if (total) {
+      addButtons(2);
+      for (let bookmark of result) {
+        addBookmark(bookmark.text, String(bookmark.index) + "X" + bookmark.id,
+          false);
+      }
+    }
+    displayMessage(browser.i18n.getMessage("messageAll", String(total)));
+  }
+
   displayMessage(browser.i18n.getMessage("messageCalculating"));
-  calculating = {};
   clearButtonsExtra();
   clearBookmarks();
   let mainFunction = calculateDupes;
   switch (mode) {
     case 0:
-      calculating.exact = true;
+      exact = true;
       // fallthrough
     case 1:
       mainFunction = calculateDupes;
       break;
-    default:
+    case 2:
       mainFunction = calculateEmpty;
+      break;
+    default:
+      mainFunction = calculateAll;
   }
-//https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/bookmarks/BookmarkTreeNode
-  return browser.bookmarks.getTree().then(mainFunction, calculateError);
+  browser.bookmarks.getTree().then(mainFunction, function (error) {
+    displayMessage(browser.i18n.getMessage("messageCalculatingError", error));
+  }).then(callback, callback);
 }
 
-function removeFinish() {
-  clearButtonsExtra();
-  clearBookmarks();
+function removeFolder(id, callback, errorCallback) {
+  browser.bookmarks.remove(id).then(callback, errorCallback);
 }
 
-function removeSuccess(a) {
-  displayMessage(browser.i18n.getMessage("messageRemoveSuccess",
-    String(a.length)));
-  removeFinish();
+function stripBookmark(indexXId, callback, errorCallback) {
+  let id, index;
+  {
+    let x = indexXId.indexOf("X");
+    index =  Number(indexXId.substring(0, x));
+    id = indexXId.substring(x + 1);
+  }
+  browser.bookmarks.get(id).then(function (nodeArray) {
+    if (!nodeArray.length) {
+      errorCallback(browser.i18n.getMessage("errorNodeNotFound"));
+      return;
+    }
+    let node = nodeArray[0];
+    let bookmark = {
+      parentId: node.parentId,
+      title: node.title,
+      url: node.url,
+      index: index,
+    };
+    if (typeof(node.type) != "undefined") {
+      bookmark.type = node.type;
+    }
+    browser.bookmarks.create(bookmark).then(function (a) {
+      browser.bookmarks.remove(id).then(callback, errorCallback);
+    }, errorCallback);
+  }, errorCallback);
 }
 
-function removeFailure(error) {
-  displayMessage(browser.i18n.getMessage("messageRemoveFailure", error));
-  removeFinish();
-}
-
-function removeMarked() {
+function processMarked(remove, callback) {
+  displayMessage(browser.i18n.getMessage("messageRemovingMarked"));
   let top = getTop();
   if (!top.hasChildNodes()) {
     return;
   }
-  let promises = new Array();
-  for (let child of top.childNodes) {
-    if (child.nodeName != "TR") {
-      continue;
-    }
-    let checkBox = getCheckBox(child);
-    if (!checkBox.checked) {
-      continue;
-    }
-    promises.push(browser.bookmarks.remove(checkBox.id));
-  }
-  return Promise.all(promises).then(removeSuccess, removeFailure);
-}
+  let current = 0;
+  let total = -1;
+  let process = (remove ? removeFolder : stripBookmark);
 
-function unlock() {
-  lock = false;
-}
+  function processRecurse(a) {
+    ++total;
+    while (current < top.childNodes.length) {
+      let child = top.childNodes[current++];
+      if (child.nodeName != "TR") {
+        continue;
+      }
+      let checkBox = getCheckBox(child);
+      if (!checkBox.checked) {
+        continue;
+      }
+      process(checkBox.id, processRecurse, function (error) {
+        displayMessage(browser.i18n.getMessage(
+          (remove ? "messageRemoveError" : "messageStripError"),
+          [error, String(total)]));
+        clearWindow();
+        callback();
+      });
+      return;
+    }
+    displayMessage(browser.i18n.getMessage(
+      (remove ? "messageRemoveSuccess" : "messageStripSuccess"),
+      String(total)));
+    clearWindow();
+    callback();
+  }
 
-function clickListener(event) {
-  if (lock || (!event.target) || !event.target.id) {
-    return;
-  }
-  lock = true;
-  switch (event.target.id) {
-    case "buttonListExactDupes":
-      calculate(0).then(unlock, unlock);
-      return;
-    case "buttonListSimilarDupes":
-      calculate(1).then(unlock, unlock);
-      return;
-    case "buttonListEmpty":
-      calculate(2).then(unlock, unlock);
-      return;
-    case "buttonRemoveMarked":
-      removeMarked().then(unlock, unlock);
-      return;
-    case "buttonMarkAll":
-      mark(2);
-      break;
-    case "buttonMarkButFirst":
-      mark(1);
-      break;
-    case "buttonMarkButLast":
-      mark(-1);
-      break;
-    case "buttonUnmarkAll":
-      mark(0);
-      break;
-  }
-  unlock();
+  processRecurse(null);
 }
 
 {
+  let lock = false;
+
+  function unlock() {
+    lock = false;
+  }
+
+  function clickListener(event) {
+    if (lock || (event.button && (event.button != 1)) ||
+      (event.buttons && (event.buttons != 1)) ||
+      (!event.target) || !event.target.id) {
+      return;
+    }
+    lock = true;
+    switch (event.target.id) {
+      case "buttonListExactDupes":
+        calculate(0, unlock);
+        return;
+      case "buttonListSimilarDupes":
+        calculate(1, unlock);
+        return;
+      case "buttonListEmpty":
+        calculate(2, unlock);
+        return;
+      case "buttonListAll":
+        calculate(3, unlock);
+        return;
+      case "buttonRemoveMarked":
+        processMarked(true, unlock);
+        return;
+      case "buttonStripMarked":
+        processMarked(false, unlock);
+        return;
+      case "buttonMarkAll":
+        mark(2);
+        break;
+      case "buttonMarkButFirst":
+        mark(1);
+        break;
+      case "buttonMarkButLast":
+        mark(-1);
+        break;
+      case "buttonUnmarkAll":
+        mark(0);
+        break;
+    }
+    unlock();
+  }
+
   let parent = document.getElementById("buttonsBase");
   addButton(parent, "buttonListExactDupes");
   addButton(parent, "buttonListSimilarDupes");
   addButton(parent, "buttonListEmpty");
+  addButton(parent, "buttonListAll");
   document.addEventListener("click", clickListener);
 }
